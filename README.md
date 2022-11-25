563b52427d39ef8da5babab91a9435d16c38de8b
Sam Ray <h.rayatnia@gmail.com>
2022-11-25 13:41:59 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KDesignSystem
+
+A description of this package.
---
+++ Tests/KDesignSystemTests/KDesignSystemTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import KDesignSystem
+
+final class KDesignSystemTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(KDesignSystem().text, "Hello, World!")
+    }
+}
