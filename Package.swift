b808747ae238b92b969fe290d484d6ca04c27fcd
Sam Ray <h.rayatnia@gmail.com>
2022-11-28 14:30:28 +0100

    add basic structure to project

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "KDesignSystem",
+    platforms: [.iOS(.v14), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,6 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/hrayatnia/KCTestUtil", from: "1.0.2"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.10.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,6 +26,7 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "KDesignSystemTests",
-            dependencies: ["KDesignSystem"]),
+            dependencies: ["KDesignSystem", "KCTestUtil",
+                           .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),]),
     ]
 )
---
+++ Tests/KDesignSystemTests/ScreenShotTests/CardViewScreenShotTest.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sam Ray on 2022-11-28.
+//
+
+import Foundation
