de91aab7562a6604e5b83870c4944c910bf87ea3
Sam Ray <h.rayatnia@gmail.com>
2022-11-28 16:03:51 +0100

    adding basic of card view

--- Sources/KDesignSystem/Components/Card/CardView.swift
+++ Sources/KDesignSystem/Components/Card/CardView.swift
@@ -1,1 +1,36 @@
+import SwiftUI
 
+struct CardView<Content>: View where Content: View {
+    private let content: () -> Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        VStack {
+            content()
+        }
+    }
+}
+
+#if DEBUG
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyListView {
+            ForEach(1..<10) { _ in
+                CardView {
+                    HStack {
+                        Text("City")
+                        Spacer()
+                        Text("11 c")
+                    }
+                }
+                .padding()
+                .background(Color.gray)
+                .modifier(CardModifier(shadow: .clear))
+            }
+        }.padding()
+    }
+}
+#endif
