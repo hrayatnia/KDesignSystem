60696da2a7d4499d224f24a947e7782bfcfd649e
Sam Ray <h.rayatnia@gmail.com>
2022-12-01 18:07:28 +0100

    add toast view

---
+++ Sources/KDesignSystem/Components/Toast/ToastModifier.swift
@@ -0,0 +1,69 @@
+import SwiftUI
+public struct Toast: ViewModifier {
+
+  public static let short: TimeInterval = 2
+  public static let long: TimeInterval = 3.5
+
+  let message: String
+  @Binding var isShowing: Bool
+  let config: Config
+
+  public func body(content: Content) -> some View {
+    ZStack {
+      content
+      toastView
+    }
+  }
+
+  private var toastView: some View {
+    VStack {
+      Spacer()
+      if isShowing {
+        Group {
+          Text(message)
+            .multilineTextAlignment(.center)
+            .foregroundColor(config.textColor)
+            .font(config.font)
+            .padding(8)
+        }
+        .background(config.backgroundColor)
+        .cornerRadius(8)
+        .onTapGesture {
+          isShowing = false
+        }
+        .onAppear {
+          DispatchQueue.main.asyncAfter(deadline: .now() + config.duration) {
+            isShowing = false
+          }
+        }
+      }
+    }
+    .padding(.horizontal, 16)
+    .padding(.bottom, 18)
+    .animation(config.animation, value: isShowing)
+    .transition(config.transition)
+  }
+
+  public struct Config {
+    let textColor: Color
+    let font: Font
+    let backgroundColor: Color
+    let duration: TimeInterval
+    let transition: AnyTransition
+    let animation: Animation
+
+    public init(textColor: Color = .white,
+         font: Font = .system(size: 14),
+         backgroundColor: Color = .black.opacity(0.588),
+         duration: TimeInterval = Toast.short,
+         transition: AnyTransition = .opacity,
+         animation: Animation = .linear(duration: 0.3)) {
+      self.textColor = textColor
+      self.font = font
+      self.backgroundColor = backgroundColor
+      self.duration = duration
+      self.transition = transition
+      self.animation = animation
+    }
+  }
+}
